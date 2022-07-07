c78ee182f5aac5945e82e618284b7a8f1f9954d7
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-07-07 20:53:07 +0000

    Fix linter errors

--- src/redux/covid/api.js
+++ src/redux/covid/api.js
@@ -1,13 +1,17 @@
+import axios from 'axios';
+
 const apiContinentsURL = 'https://corona.lmao.ninja/v2/continents';
 const apiCountriesURL = 'https://corona.lmao.ninja/v2/countries';
 
 const fetchAllContinents = async () => {
-  const response = await fetch(apiContinentsURL).then((data) => data.json());
+  const response = await axios
+    .get(apiContinentsURL)
+    .then((data) => data.json());
   return response;
 };
 
 const fetchAllCountries = async () => {
-  const response = await fetch(apiCountriesURL).then((data) => data.json());
+  const response = await axios.get(apiCountriesURL).then((data) => data.json());
   return response;
 };
 
